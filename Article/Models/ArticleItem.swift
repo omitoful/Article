a40b4e5f959ece0dcd85b7e39a4679f90099c462
omitoful <67890951+omitoful@users.noreply.github.com>
2021-03-30 11:14:28 +0800

    try Firebase

---
+++ Article/Models/ArticleItem.swift
@@ -0,0 +1,18 @@
+//
+//  Articles.swift
+//  Article
+//
+//  Created by 陳冠甫 on 2021/3/30.
+//
+
+import Foundation
+import Firebase
+
+struct ArtileItem {
+    
+    let title: String
+    let content: String
+    let author: String
+    let date: String
+    var completed: Bool
+}
